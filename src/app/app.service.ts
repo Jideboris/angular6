8e3727e387906e399e843f99714afb573c9666e2
Anthony Borisade <anthonyborisade@anthonys-mbp.home>
2018-07-01 18:34:38 +0100

    use of inplace of obserbableof

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,5 +1,5 @@
-import { Injectable } from "@angular/core";
-import { Observable} from "rxjs"
+import { Injectable } from "@angular/core"
+import { of } from "rxjs"
 import { User } from "./model/user"
 
 
@@ -19,7 +19,8 @@ export class AppService {
     { id: 1, firstName: 'Hary', lastName: 'Pan', email: 'hary@gmail.com' },
     { id: 1, firstName: 'praks', lastName: 'pb', email: 'praks@gmail.com' },
     ];
-    return Observable.of(fakeUsers).delay(5000)
+    
+    return of(fakeUsers)
 
   
   }
