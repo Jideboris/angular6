1b14c6a6c55663868f6a6f39c6261d30f4b72a66
Anthony Borisade <anthonyborisade@anthonys-mbp.home>
2018-07-02 16:17:21 +0100

    schlumber_test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser'
 import { NgModule } from '@angular/core'
 import { AppRoutingModule } from '../app/app.routing.module'
-import {FormsModule} from '@angular/forms'
+import { FormsModule } from '@angular/forms'
+
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppComponent } from './app.component'
-import { LoginComponent } from './login/login.component'
 import { UserComponent } from './user/user.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
     UserComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,6 +1,8 @@
+
+import { User } from "./model/user"
+import { HttpClient } from "@angular/common/http"
 import { Injectable } from "@angular/core"
-import { of } from "rxjs"
-import { HttpClient } from '@angular/common/http'
+import { of, Observable } from "rxjs"
  
 
 @Injectable({
@@ -8,18 +10,17 @@ import { HttpClient } from '@angular/common/http'
 })
 export class AppService {
    url = 'https://jsonplaceholder.typicode.com/users'
-  constructor() { }
-  getUsers(){
-    let fakeUsers = [{ id: 1, name: 'Leanne Graham', username: 'Bret', email: 'Sincere@april.biz' },
-    { id: 2, name: 'Ervin Howell', username: 'Antonette', email: 'Shanna@melissa.tv' },
-    { id: 3, name: 'Clementine Bauch', username: 'Samantha', email: 'Nathan@yesenia.net' },
-    { id: 4, name: 'Patricia Lebsack', username: 'Karianne', email: 'Julianne.OConner@kory.org' },
-    ];
-    return of(fakeUsers)
-  }
-  
-  // getLiveUsers(): Observable<User[]> {
-  //   return this.http.get<User[]>(this.url)
+  constructor(private http: HttpClient) { }
+  // getUsers(){
+  //   let fakeUsers = [{ id: 1, name: 'Leanne Graham', username: 'Bret', email: 'Sincere@april.biz' },
+  //   { id: 2, name: 'Ervin Howell', username: 'Antonette', email: 'Shanna@melissa.tv' },
+  //   { id: 3, name: 'Clementine Bauch', username: 'Samantha', email: 'Nathan@yesenia.net' },
+  //   { id: 4, name: 'Patricia Lebsack', username: 'Karianne', email: 'Julianne.OConner@kory.org' },
+  //   ];
+  //   return of(fakeUsers)
   // }
+  getUsers(): Observable<User[]> {
+    return this.http.get<User[]>(this.url)
+  }
 
 }
